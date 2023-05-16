b057a7aba996aca7652c5067c8f352410d210f58
himanish-tecblic <himanishvyas.tecblic@gmail.com>
2023-05-16 19:19:58 +0530

    first user created

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    
+    name = models.CharField(max_length=100)
+    roll = models.CharField(max_length=100)
+    
+    
